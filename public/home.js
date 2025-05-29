55689c94690b3f578cbd5640e7807200b4bd0083
okpako12 <lunofraudrecovery@gmail.com>
2025-05-29 16:13:18 +0100

    Update home.js

--- public/home.js
+++ public/home.js
@@ -117,7 +117,7 @@ document.addEventListener("DOMContentLoaded", () => {
           <div class="placeholder-content">
             <i class="fas fa-home fa-3x"></i>
             <h3>Home Dashboard</h3>
-            <p>This is your personal dashboard. View your account information and settings here.</p>
+            <p>Our mission is to increase economic freedom in the world. Join us and make an impact at a global scale.</p>
           </div>
         `
         break
